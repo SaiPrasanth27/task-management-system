74ba256783eb1c89cf6ad5c118615db4b5a8e354
Prasanth <prasanthtamarapalli27@gmail.com>
2026-03-26 10:29:58 +0530

    Added utils for handling the frontend, backend connection

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api',
+});
+
+// Add a request interceptor to attach the token
+api.interceptors.request.use((config) => {
+  const userString = localStorage.getItem('user');
+  if (userString) {
+    const user = JSON.parse(userString);
+    if (user && user.token) {
+      config.headers.Authorization = `Bearer ${user.token}`;
+    }
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export default api;
